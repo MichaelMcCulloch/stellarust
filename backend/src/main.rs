9d5218df7ba58f33e95f603744c7caf2c467f62a
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2021-12-14 14:30:36 -0700

    add a test

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,8 +1,7 @@
-use std::panic;
-
 use actix_cors::Cors;
 use actix_web::{get, middleware, web::Data, App, HttpResponse, HttpServer, Responder};
 use listenfd::ListenFd;
+use std::panic;
 
 #[get("/")]
 pub async fn index(player_data: Data<Vec<i32>>) -> impl Responder {
@@ -33,4 +32,29 @@ async fn main() -> std::io::Result<()> {
     };
 
     server.run().await
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::index;
+    use actix_web::{
+        body::Body,
+        test,
+        web::{self, Data},
+        App,
+    };
+    use serde_json::json;
+
+    #[actix_rt::test]
+    async fn test_index_returnsJsonVec0() {
+        let data = Data::new(vec![0]);
+        let mut app = test::init_service(App::new().app_data(data.clone()).service(index)).await;
+        let req = test::TestRequest::with_header("content-type", "application/json").to_request();
+        let mut resp = test::call_service(&mut app, req).await;
+        let body = resp.take_body();
+        let body = body.as_ref().unwrap();
+        assert!(resp.status().is_success());
+        assert_eq!(&Body::from(json!(vec![0])), body);
+    }
 }
