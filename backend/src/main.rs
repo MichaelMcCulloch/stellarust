3d43cdae8a2dd3cd8656e02a6f001243a5a468f6
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2021-12-14 12:32:10 -0700

    run the backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use std::panic;
+
+use actix_cors::Cors;
+use actix_web::{get, middleware, web::Data, App, HttpResponse, HttpServer, Responder};
+use listenfd::ListenFd;
+
+#[get("/")]
+pub async fn index(player_data: Data<Vec<i32>>) -> impl Responder {
+    let data = player_data.get_ref().clone();
+    HttpResponse::Ok().json(data)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "info");
+    env_logger::init();
+
+    let data = Data::new(vec![0]);
+
+    let mut server = HttpServer::new(move || {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .wrap(Cors::default())
+            .app_data(data.clone())
+            .service(index)
+    });
+
+    server = if let Some(listener) = ListenFd::from_env().take_tcp_listener(0)? {
+        log::info!("{:?}", listener);
+        server.listen(listener)?
+    } else {
+        panic!()
+    };
+
+    server.run().await
 }
