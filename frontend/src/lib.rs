7c82cc1083a11d749828f2813047b3113d128c7c
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2021-12-14 18:28:35 -0700

    front end test

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -5,16 +5,7 @@ pub(crate) mod app;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(start)]
-pub fn main() {
+pub fn start() {
     wasm_logger::init(wasm_logger::Config::default());
     yew::start_app::<app::App>();
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
 }
