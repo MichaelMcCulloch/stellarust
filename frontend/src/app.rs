931acb366c9b2735e3a6e95094b5fbb17e6619f1
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2021-12-16 12:31:10 -0700

    app state

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -1,7 +1,14 @@
-use yew::{html, Component};
+use yew::{html, Component, Context, Html};
+
+use crate::fetch::FetchState;
+
+#[derive(Clone)]
+pub struct AppState {
+    data: FetchState<Vec<u32>>,
+}
 
 pub struct App {
-    str: String,
+    state: AppState,
 }
 pub enum Msg {
     Change,
@@ -13,40 +20,54 @@ impl Component for App {
     type Properties = ();
 
     fn create(_ctx: &yew::Context<Self>) -> Self {
-        Self {
-            str: String::from("Hello World!"),
-        }
+        App::new()
     }
 
     fn view(&self, ctx: &yew::Context<Self>) -> yew::Html {
-        html! {
-            <>
-                <button
-                    class="whatever"
-                    onclick={ctx.link().callback(|_| Msg::Change)}>
-                    {"Click Me!"}
-                </button>
-                <p class="label"> {&self.str} </p>
-            </>
-        }
+        App::view_app()
     }
     fn changed(&mut self, _ctx: &yew::Context<Self>) -> bool {
         todo!()
     }
     fn update(&mut self, _ctx: &yew::Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            Msg::Change => {
-                self.str = String::from("Goodbye World");
-                true
-            }
+            Msg::Change => true,
+        }
+    }
+}
+
+impl App {
+    fn new() -> Self {
+        Self {
+            state: AppState {
+                data: FetchState::NotFetching,
+            },
+        }
+    }
+    fn view_app() -> Html {
+        html! {
+            <div>
+                <label id="number" class="number">{"0"}</label>
+            </div>
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use wasm_bindgen_test::*;
     use yew::{html, FunctionComponent, FunctionProvider, Properties};
+
+    trait TestAppState {
+        fn getState(&self) -> AppState;
+    }
+
+    impl TestAppState for App {
+        fn getState(&self) -> AppState {
+            self.state.clone()
+        }
+    }
 
     wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
 
@@ -95,5 +116,30 @@ mod tests {
         let result = obtain_result();
 
         assert_eq!(result.as_str(), "done");
+    }
+
+    #[wasm_bindgen_test]
+    fn view_shows_0() {
+        yew::start_app_with_props_in_element::<App>(
+            gloo_utils::document().get_element_by_id("output").unwrap(),
+            (),
+        );
+
+        let number_field = gloo_utils::document()
+            .get_element_by_id("number")
+            .expect("No field with id 'number'");
+
+        let field_text = number_field.text_content().unwrap();
+
+        assert_eq!(field_text.as_str(), "0")
+    }
+
+    #[wasm_bindgen_test]
+    fn app_create_state_is_not_fetching() {
+        let app = App::new();
+
+        let data = app.getState().data;
+
+        assert_eq!(data, FetchState::NotFetching);
     }
 }
