384641bbfbb0f785485f44669800d7cb2e82bc51
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2021-12-17 16:30:06 -0700

    add route for save select

--- frontend/src/route.rs
+++ frontend/src/route.rs
@@ -1,10 +1,12 @@
-use crate::pages::empire_select::EmpireSelect;
+use crate::pages::{empire_select::EmpireSelect, save_game_select::SaveGameSelect};
 use yew::prelude::*;
 use yew_router::prelude::*;
 
 #[derive(Routable, PartialEq, Clone, Debug)]
 pub enum Route {
     #[at("/")]
+    SaveGameSelect,
+    #[at("/empires")]
     EmpireSelect,
 }
 
@@ -12,6 +14,9 @@ pub fn switch(routes: &Route) -> Html {
     match routes.clone() {
         Route::EmpireSelect => {
             html! { <EmpireSelect /> }
+        }
+        Route::SaveGameSelect => {
+            html! { <SaveGameSelect/>}
         }
     }
 }
