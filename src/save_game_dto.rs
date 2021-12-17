72b0e07b62e2c863c9fc2d02111078694798eea6
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2021-12-17 16:28:43 -0700

    Add save game select screen and dto object, with tests

---
+++ src/save_game_dto.rs
@@ -0,0 +1,9 @@
+use serde::{Deserialize, Serialize};
+use time::OffsetDateTime;
+
+#[derive(Deserialize, Serialize)]
+pub struct SaveGameDto {
+    pub empires: Vec<String>,
+    pub last_save_zoned_date_time: OffsetDateTime,
+}
+fn wh() {}
