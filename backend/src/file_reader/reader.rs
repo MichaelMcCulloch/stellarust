6d6eb40197b1a3d501bf59c16b12d7d952871a16
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2021-12-17 21:45:04 -0700

    connect the file reader to the server and the int test kinda works now

--- backend/src/file_reader/reader.rs
+++ backend/src/file_reader/reader.rs
@@ -2,7 +2,7 @@ use std::path::PathBuf;
 
 use stellarust::dto::SaveGameDto;
 
-pub(crate) trait FileReader {
+pub trait FileReader {
     fn read_from_path(path: &PathBuf) -> Vec<SaveGameDto>;
     fn read() -> Vec<SaveGameDto>;
 }
