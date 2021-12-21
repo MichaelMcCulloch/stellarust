0b4386ad0dffa1713b8cdb5cddf94fd30d5fc09a
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2021-12-21 16:13:59 -0700

    green test for directory watcher, files after load

--- backend/tests/dirwatcher.rs
+++ backend/tests/dirwatcher.rs
@@ -13,7 +13,7 @@ fn get_resource_dir() -> PathBuf {
 }
 
 fn get_test_file_paths() -> Vec<PathBuf> {
-    let file_paths: Vec<PathBuf> = vec!["f1", "f2", "f3"]
+    let file_paths: Vec<PathBuf> = vec!["f1, f2, f3"]
         .into_iter()
         .map(|file_name| {
             let mut res = get_resource_dir();
@@ -28,29 +28,34 @@ fn get_test_file_paths() -> Vec<PathBuf> {
 mod tests {
 
     use super::*;
-    use backend::dirwatcher::DirectoryEventHandler;
-    use std::fs;
+    use backend::{dirwatcher::DirectoryEventHandler, model::CustodianMsg};
+    use std::{fs, io::Write, thread, time::Duration};
 
     #[test]
     fn test_dir_watcher__oncreate__existing_files_are_in_receiver_queue() {
-        let file_paths = get_test_file_paths();
-
-        for (index, path) in file_paths.clone().into_iter().enumerate() {
-            fs::write(path, format!("{}", index)).unwrap();
-        }
-
-        let (event_receiver, _watcher) = DirectoryEventHandler::create(&get_resource_dir());
-        for event in event_receiver.into_iter() {
-            println!("{:?}", event)
-        }
-
-        for path in file_paths {
-            fs::remove_file(path).unwrap();
-        }
+        let _new_files = vec!["f1, f2, f3"];
     }
 
     #[test]
     fn test_dir_watcher__receiver__new_files_are_in_receiver_queue() {
-        let _new_files = vec!["f1, f2, f3"];
+        let file_paths = get_test_file_paths();
+
+        let dir = get_resource_dir();
+
+        let (msg_receiver, _watcher) = DirectoryEventHandler::create(&dir);
+
+        thread::sleep(Duration::from_secs(1));
+        for (index, path) in file_paths.clone().into_iter().enumerate() {
+            fs::write(path, format!("{}", index)).unwrap();
+        }
+
+        let result = msg_receiver.recv_timeout(Duration::from_secs(10));
+        for path in file_paths {
+            fs::remove_file(path).unwrap();
+        }
+        match result {
+            Ok(msg) => assert_eq!(CustodianMsg::Data(0), msg),
+            Err(error) => assert!(false),
+        }
     }
 }
