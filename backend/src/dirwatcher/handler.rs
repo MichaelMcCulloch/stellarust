6a46593087b075f97b2861dcf5caaefdc3d77dcc
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2021-12-21 17:06:41 -0700

    one test for the thing bc concurrency bad

--- backend/src/dirwatcher/handler.rs
+++ backend/src/dirwatcher/handler.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     path::PathBuf,
     sync::mpsc::{channel, Receiver, Sender},
     thread,
@@ -24,6 +25,14 @@ impl DirectoryEventHandler {
 
         let me = DirectoryEventHandler { watcher };
 
+        let existant_files = get_existing_files(directory);
+
+        for path in existant_files {
+            custodian_message_sender
+                .send(CustodianMsg::Data(Parser::from_file(&path)))
+                .unwrap();
+        }
+
         me.start_directory_event_handler(raw_event_receiver, custodian_message_sender);
         (custodian_message_receiver, me)
     }
@@ -31,10 +40,10 @@ impl DirectoryEventHandler {
     fn start_directory_event_handler(
         &self,
         raw_event_receiver: Receiver<RawEvent>,
-        pathbuf_sender: Sender<CustodianMsg>,
+        custodian_message_sender: Sender<CustodianMsg>,
     ) {
         thread::spawn(move || loop {
-            match forward_event_to_path(&raw_event_receiver, &pathbuf_sender) {
+            match forward_event_to_path(&raw_event_receiver, &custodian_message_sender) {
                 Err(e) => log::error!("{}", e),
                 _ => continue,
             }
@@ -42,18 +51,29 @@ impl DirectoryEventHandler {
     }
 }
 
+fn get_existing_files(path: &PathBuf) -> Vec<PathBuf> {
+    fs::read_dir(path)
+        .unwrap()
+        .into_iter()
+        .filter_map(|result| match result {
+            Ok(dir_entry) => Some(dir_entry.path()),
+            Err(_) => None,
+        })
+        .collect()
+}
+
 fn forward_event_to_path(
-    raw_event_receiver: &Receiver<RawEvent>,
-    pathbuf_sender: &Sender<CustodianMsg>,
+    receiver: &Receiver<RawEvent>,
+    sender: &Sender<CustodianMsg>,
 ) -> Result<(), anyhow::Error> {
-    let event = raw_event_receiver.recv()?;
+    let event = receiver.recv()?;
 
     (match event {
         RawEvent {
             op: Ok(Op::CLOSE_WRITE),
             path: Some(path),
             cookie: _cookie,
-        } => pathbuf_sender.send(CustodianMsg::Data(Parser::from_file(&path))),
+        } => sender.send(CustodianMsg::Data(Parser::from_file(&path))),
         _ => Ok(()),
     })?;
 
--- backend/tests/dirwatcher.rs
+++ backend/tests/dirwatcher.rs
@@ -1,29 +1,5 @@
 use std::path::PathBuf;
 
-fn get_resource_dir() -> PathBuf {
-    let test_resource_dir = {
-        let mut dir: PathBuf = PathBuf::from(std::env::current_dir().unwrap());
-        dir.pop();
-        dir.push("res");
-        dir.push("test_data");
-        dir.push("dirwatcher");
-        dir
-    };
-    test_resource_dir
-}
-
-fn get_test_file_paths() -> Vec<PathBuf> {
-    let file_paths: Vec<PathBuf> = vec!["f1, f2, f3"]
-        .into_iter()
-        .map(|file_name| {
-            let mut res = get_resource_dir();
-            res.push(file_name);
-            res
-        })
-        .collect();
-    file_paths
-}
-
 #[cfg(test)]
 mod tests {
 
@@ -35,29 +11,60 @@ mod tests {
     use std::{fs, io::Write, thread, time::Duration};
 
     #[test]
-    fn test_dir_watcher__oncreate__existing_files_are_in_receiver_queue() {
-        let _new_files = vec!["f1, f2, f3"];
-    }
-
-    #[test]
     fn test_dir_watcher__receiver__new_files_are_in_receiver_queue() {
-        let file_paths = get_test_file_paths();
-
         let dir = get_resource_dir();
 
-        let (msg_receiver, _watcher) = DirectoryEventHandler::create(&dir);
-
-        for (index, path) in file_paths.clone().into_iter().enumerate() {
+        let existing_files = get_test_file_paths(0);
+        for (index, path) in existing_files.clone().into_iter().enumerate() {
             fs::write(path, format!("{}", index)).unwrap();
         }
 
-        let result = msg_receiver.recv_timeout(Duration::from_secs(10));
-        for path in file_paths {
+        let (msg_receiver, _watcher) = DirectoryEventHandler::create(&dir);
+
+        msg_receiver.recv_timeout(Duration::from_secs(2)).unwrap();
+        msg_receiver.recv_timeout(Duration::from_secs(2)).unwrap();
+        msg_receiver.recv_timeout(Duration::from_secs(2)).unwrap();
+
+        let new_files = get_test_file_paths(3);
+        for (index, path) in new_files.clone().into_iter().enumerate() {
+            fs::write(path, format!("{}", index)).unwrap();
+        }
+
+        let result = msg_receiver.recv_timeout(Duration::from_secs(2));
+        for path in vec![existing_files, new_files].concat() {
             fs::remove_file(path).unwrap();
         }
         match result {
             Ok(msg) => assert_eq!(CustodianMsg::Data(ModelDataPoint { data: 0 }), msg),
             Err(error) => panic!("{}", error),
         }
+    }
+
+    fn get_resource_dir() -> PathBuf {
+        let test_resource_dir = {
+            let mut dir: PathBuf = PathBuf::from(std::env::current_dir().unwrap());
+            dir.pop();
+            dir.push("res");
+            dir.push("test_data");
+            dir.push("dirwatcher");
+            dir
+        };
+        test_resource_dir
+    }
+
+    fn get_test_file_paths(start_at: usize) -> Vec<PathBuf> {
+        let file_paths: Vec<PathBuf> = vec![
+            format!("f{}", start_at),
+            format!("f{}", start_at + 1),
+            format!("f{}", start_at + 2),
+        ]
+        .into_iter()
+        .map(|file_name| {
+            let mut res = get_resource_dir();
+            res.push(file_name);
+            res
+        })
+        .collect();
+        file_paths
     }
 }
