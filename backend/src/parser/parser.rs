4af3447a8b81329714f1dc1c0fb76192c83936ed
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2021-12-21 20:50:49 -0700

    move parser impl

---
+++ backend/src/parser/parser.rs
@@ -0,0 +1,33 @@
+use std::path::PathBuf;
+
+use crate::model::ModelDataPoint;
+use crate::unzipper::Unzipper;
+
+pub struct Parser {}
+
+impl Parser {
+    pub fn from_file(path: &PathBuf) -> ModelDataPoint {
+        let (meta, gamestate) = Unzipper::get_zipped_content(&path).unwrap();
+
+        Parser::from_meta(meta.as_str());
+        Parser::from_gamestate(gamestate.as_str());
+
+        ModelDataPoint { data: 0 }
+    }
+    pub fn from_meta(string: &str) -> ModelDataPoint {
+        // let i: usize = string.parse().unwrap();
+        ModelDataPoint { data: 0 }
+    }
+    pub fn from_gamestate(string: &str) -> ModelDataPoint {
+        // let i: usize = string.parse().unwrap();
+        ModelDataPoint { data: 0 }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn from_string() {}
+}
