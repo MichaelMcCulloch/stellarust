96d6de71336f813a971e1105e8863a7678b8ce67
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2022-01-04 16:27:20 -0700

    parse req dlcs from meta

--- backend/src/parser/parser.rs
+++ backend/src/parser/parser.rs
@@ -2,7 +2,7 @@ use crate::unzipper::Unzipper;
 use anyhow::Result;
 use clausewitz_parser::clausewitz::root::root;
 use clausewitz_parser::clausewitz::Val;
-use data_model::ModelDataPoint;
+use data_model::{EmpireData, ModelDataPoint};
 use std::error::Error;
 use std::fmt;
 use std::fmt::{Debug, Display, Formatter};
@@ -67,14 +67,86 @@ impl Parser {
 }
 
 impl From<ParseResult<'_>> for ModelDataPoint {
-    fn from(_: ParseResult<'_>) -> Self {
+    fn from(result: ParseResult<'_>) -> Self {
+        let meta = result.meta;
+        let gamestate = result.gamestate;
+
+        let required_dlcs = get_required_dlcs_from_meta(&meta);
+
+        let empire_list = get_empires_from_gamestate(gamestate);
+
         todo!()
     }
 }
 
+fn get_required_dlcs_from_meta(meta: &Val) -> Vec<String> {
+    if let Val::Dict(pairs) = meta {
+        let required_dlcs = pairs
+            .into_iter()
+            .filter_map(|(k, v)| if *k == "required_dlcs" { Some(v) } else { None })
+            .next()
+            .unwrap();
+        if let Val::Set(dlc_list) = required_dlcs {
+            return dlc_list
+                .into_iter()
+                .filter_map(|val| {
+                    if let Val::StringLiteral(string_litteral) = val {
+                        Some(String::from(*string_litteral))
+                    } else {
+                        None
+                    }
+                })
+                .collect();
+        } else {
+            unimplemented!("This should be an array")
+        }
+    } else {
+        unimplemented!("This should be an array")
+    }
+}
+
+fn get_empires_from_gamestate(gamestate: Val) -> Vec<EmpireData> {
+    todo!()
+}
+
 #[cfg(test)]
 mod tests {
+    use std::{fs, path::PathBuf};
+
+    use clausewitz_parser::clausewitz::root::root;
+
+    use super::get_required_dlcs_from_meta;
 
     #[test]
-    fn from_() {}
+    fn get_required_dlcs_from_meta__meta__returns_dlc_list() {
+        let home = std::env::var("HOME").unwrap();
+        let ext = "Dev/stellarust/res/test_data/campaign_raw/unitednationsofearth_-15512622/autosave_2200.02.01/meta";
+        let meta_path = PathBuf::from_iter(vec![home.as_str(), ext]);
+        let meta_string = fs::read_to_string(meta_path).unwrap();
+
+        let (_, parse) = root(&meta_string.as_str()).unwrap();
+
+        let dlcs = get_required_dlcs_from_meta(&parse);
+
+        assert_eq!(
+            dlcs,
+            vec![
+                "Ancient Relics Story Pack",
+                "Anniversary Portraits",
+                "Apocalypse",
+                "Distant Stars Story Pack",
+                "Federations",
+                "Horizon Signal",
+                "Humanoids Species Pack",
+                "Leviathans Story Pack",
+                "Lithoids Species Pack",
+                "Megacorp",
+                "Necroids Species Pack",
+                "Nemesis",
+                "Plantoids Species Pack",
+                "Synthetic Dawn Story Pack",
+                "Utopia"
+            ]
+        );
+    }
 }
--- clausewitz-parser/src/main.rs
+++ clausewitz-parser/src/main.rs
@@ -4,7 +4,7 @@ use clausewitz_parser::clausewitz::root::par_root;
 use memmap::Mmap;
 
 fn main() {
-    let filename = "/home/michael/Dev/stellarust/res/test_data/campaign_raw/unitednationsofearth_-15512622/autosave_2200.02.01/gamestate_large2";
+    let filename = "/home/michael/Dev/stellarust/res/test_data/campaign_raw/unitednationsofearth_-15512622/autosave_2200.02.01/meta";
     let file = File::open(filename).expect("File not found");
     let mmap = unsafe { Mmap::map(&file).expect(&format!("Error mapping file {:?}", file)) };
 
@@ -12,19 +12,21 @@ fn main() {
 
     let s1 = Instant::now();
     let prepared_input = str.replace("\n}\n", "\n}\n#");
-    println!("{}", s1.elapsed().as_millis());
+    // println!("{}", s1.elapsed().as_millis());
 
     let start = Instant::now();
-    let _ = par_root(prepared_input.as_str());
+    let result = par_root(prepared_input.as_str());
 
-    let end = start.elapsed();
+    // let end = start.elapsed();
 
-    let size_in_bytes = file.metadata().unwrap().size();
-    let speed = (size_in_bytes as u128 / end.as_millis()) * 1000;
+    // let size_in_bytes = file.metadata().unwrap().size();
+    // let speed = (size_in_bytes as u128 / end.as_millis()) * 1000;
 
-    println!(
-        "{:?}MB/s, took {} seconds.",
-        speed as f32 / 1000000 as f32,
-        end.as_secs()
-    );
+    // println!(
+    //     "{:?}MB/s, took {} seconds.",
+    //     speed as f32 / 1000000 as f32,
+    //     end.as_secs()
+    // );
+
+    println!("{}", result.unwrap().1);
 }
