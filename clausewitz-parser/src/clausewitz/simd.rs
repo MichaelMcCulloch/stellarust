0a48a9e473a688d82b17849c6fadb542d1c94e75
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2022-01-08 13:32:06 -0700

    fixed length for ranges

--- clausewitz-parser/src/clausewitz/simd.rs
+++ clausewitz-parser/src/clausewitz/simd.rs
@@ -1,29 +1,31 @@
 use nom::bytes::complete::take_while;
 
 use std::arch::x86_64::{
-    _mm_cmpestri, _mm_loadu_si128, _SIDD_CMP_RANGES, _SIDD_LEAST_SIGNIFICANT, _SIDD_UBYTE_OPS,
+    _mm_cmpestri, _mm_cmpistri, _mm_loadu_si128, _SIDD_CMP_RANGES, _SIDD_LEAST_SIGNIFICANT,
+    _SIDD_UBYTE_OPS,
 };
 use std::cmp::min;
 
 //the range of all the characters which should be REJECTED
-pub const SPACE_RANGES: &[u8] = &[b'\x00', b'\x08', b'\x0e', b'\x1f', b'!', b'\xff'];
-
-pub const TOKEN_RANGES: &[u8] = &[
-    b'\x00', b'\x3c', b'\x3e', b'\x7a', b'\x7c', b'\x7c', b'\x7e', b'\xff',
+pub const SPACE_RANGES: &[u8; 16] = &[
+    b'\x00', b'\x08', b'\x0e', b'\x1f', b'!', b'\xff', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00',
+    b'\x00', b'\x00', b'\x00', b'\x00', b'\x00',
 ];
-pub const NOT_TOKEN_RANGES: &[u8] = &[b'=', b'=', b'{', b'{', b'}', b'}'];
-
-pub const NUMBER_RANGES: &[u8] = &[b'\x00', b'\x2f', b':', b'\xff'];
-pub const ALPHABET_RANGES: &[u8] = &[b'\x00', b'@', b'[', b'`', b'{', b'\xff'];
-pub const STRING_LITTERAL_CONTENT_RANGES: &[u8] = &[
+pub const NOT_TOKEN_RANGES: &[u8; 16] = &[
+    b'=', b'=', b'{', b'{', b'}', b'}', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00',
+    b'\x00', b'\x00', b'\x00', b'\x00',
+];
+pub const STRING_LITTERAL_CONTENT_RANGES: &[u8; 16] = &[
     b'\x00', b'\x08', b'\x0e', b'\x1f', b'"', b'"', b'=', b'=', b'{', b'{', b'}', b'}', b'\x7f',
-    b'\xff',
-];
-pub const IDENTIFIER_RANGES: &[u8] = &[
-    b'\x00', b'\x2d', b'\x2f', b'\x2f', b':', b'@', b'[', b'^', b'`', b'`', b'{', b'\xff',
+    b'\xff', b'\x00', b'\x00',
 ];
 
-const SIMD_RANGE: usize = 16;
+pub const IDENTIFIER_RANGES: &[u8; 16] = &[
+    b'\x00', b'\x2d', b'\x2f', b'\x2f', b':', b'@', b'[', b'^', b'`', b'`', b'{', b'\xff', b'\x00',
+    b'\x00', b'\x00', b'\x00',
+];
+
+const CHUNK_SIZE: usize = 16;
 
 use nom::error::ParseError;
 
@@ -31,26 +33,24 @@ use super::Res;
 
 pub fn take_while_simd<'a, Condition, Error: ParseError<&'a str>>(
     cond: Condition,
-    ranges: &'static [u8],
+    ranges: &'static [u8; 16],
 ) -> impl Fn(&'a str) -> Res<&'a str, &'a str>
 where
     Condition: Fn(char) -> bool,
 {
-    // move |i: &'a str| take_while_unrolled(i, |c| !cond(c))
     move |input: &'a str| {
         let condition = |c| cond(c);
-        let len = input.len();
-        if len == 0 {
+        if input.len() == 0 {
             return Ok(("", ""));
-        } else if len >= 16 {
-            simd_loop16(input, ranges, len)
+        } else if input.len() >= 16 {
+            simd_loop16(input, ranges)
         } else {
             take_while(condition)(input)
         }
     }
 }
 
-fn simd_loop16<'a>(str: &'a str, ranges: &[u8], len: usize) -> Res<&'a str, &'a str> {
+fn simd_loop16<'a>(str: &'a str, ranges: &[u8; 16]) -> Res<&'a str, &'a str> {
     let start = str.as_ptr() as usize;
     let mut i = str.as_ptr() as usize;
     let ranges16 = unsafe { _mm_loadu_si128(ranges.as_ptr() as *const _) };
@@ -61,7 +61,7 @@ fn simd_loop16<'a>(str: &'a str, ranges: &[u8], len: usize) -> Res<&'a str, &'a 
         let idx = unsafe {
             _mm_cmpestri(
                 ranges16,
-                ranges_len,
+                16,
                 s1,
                 16,
                 _SIDD_LEAST_SIGNIFICANT | _SIDD_CMP_RANGES | _SIDD_UBYTE_OPS,
@@ -75,7 +75,7 @@ fn simd_loop16<'a>(str: &'a str, ranges: &[u8], len: usize) -> Res<&'a str, &'a 
         i += 16;
     }
     let index = i - start;
-    let (before, after) = str.split_at(min(index, len));
+    let (before, after) = str.split_at(min(index, str.len()));
     return Ok((after, before));
 }
 
@@ -94,6 +94,36 @@ mod tests {
             take_while_simd::<'_, _, VerboseError<&str>>(is_space, ranges)(text).unwrap();
         assert_eq!(remainder, "|Stop this is a big long string");
         assert_eq!(parsed, " \t\n\r");
+    }
+
+    #[test]
+    fn take_while_simd__16_character_string__whitespace_collected_remainder_returned() {
+        let text = "1111111111111111";
+        let ranges = SPACE_RANGES;
+        let (remainder, parsed) =
+            take_while_simd::<'_, _, VerboseError<&str>>(is_space, ranges)(text).unwrap();
+        assert_eq!(remainder, "1111111111111111");
+        assert_eq!(parsed, "");
+    }
+
+    #[test]
+    fn take_while_simd__16_newlines_1_1__whitespace_collected_remainder_returned() {
+        let text = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n1";
+        let ranges = SPACE_RANGES;
+        let (remainder, parsed) =
+            take_while_simd::<'_, _, VerboseError<&str>>(is_space, ranges)(text).unwrap();
+        assert_eq!(remainder, "1");
+        assert_eq!(parsed, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    }
+
+    #[test]
+    fn take_while_simd__17_newlines_1_1__whitespace_collected_remainder_returned() {
+        let text = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n1";
+        let ranges = SPACE_RANGES;
+        let (remainder, parsed) =
+            take_while_simd::<'_, _, VerboseError<&str>>(is_space, ranges)(text).unwrap();
+        assert_eq!(remainder, "1");
+        assert_eq!(parsed, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
     }
 
     #[test]
--- clausewitz-parser/src/main.rs
+++ clausewitz-parser/src/main.rs
@@ -4,27 +4,25 @@ use clausewitz_parser::root;
 use memmap::Mmap;
 
 fn main() {
-    let filename = "/home/michael/Dev/stellarust/res/test_data/campaign_raw/unitednationsofearth_-15512622/autosave_2200.02.01/meta";
+    let filename = "/home/michael/Dev/stellarust/res/test_data/campaign_raw/unitednationsofearth_-15512622/autosave_2200.02.01/gamestate";
     let file = File::open(filename).expect("File not found");
     let mmap = unsafe { Mmap::map(&file).expect(&format!("Error mapping file {:?}", file)) };
 
     let str = std::str::from_utf8(&mmap[..]).unwrap();
 
-    // println!("{}", s1.elapsed().as_millis());
-
     let start = Instant::now();
     let result = root(str);
 
-    // let end = start.elapsed();
+    let end = start.elapsed();
 
-    // let size_in_bytes = file.metadata().unwrap().size();
-    // let speed = (size_in_bytes as u128 / end.as_millis()) * 1000;
+    let size_in_bytes = file.metadata().unwrap().size();
+    let speed = (size_in_bytes as u128 / end.as_millis()) * 1000;
 
-    // println!(
-    //     "{:?}MB/s, took {} seconds.",
-    //     speed as f32 / 1000000 as f32,
-    //     end.as_secs()
-    // );
+    println!(
+        "{:?}MB/s, took {} ms.",
+        speed as f32 / 1000000 as f32,
+        end.as_millis()
+    );
 
-    println!("{}", result.unwrap().1);
+    // println!("{}", result.unwrap().1);
 }
