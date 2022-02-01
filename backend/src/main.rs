47f1f2e5990e613f817f7461f1de298cda74a189
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2022-02-01 12:19:31 -0700

    move the api to the new module

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,21 +1,11 @@
 use actix_cors::Cors;
-use actix_web::{get, middleware, web::Data, App, HttpResponse, HttpServer, Responder};
-use backend::{campaign_select::selector::CampaignSelector, dirwatcher::DirectoryEventHandler};
+use actix_web::{middleware, web::Data, App, HttpResponse, HttpServer, Responder};
+use backend::{
+    api::empires, campaign_select::selector::CampaignSelector, dirwatcher::DirectoryEventHandler,
+};
 use data_model::ModelCustodian;
 use listenfd::ListenFd;
 use std::{panic, path::PathBuf, process::exit};
-
-#[get("/empires")]
-pub async fn empires(model_custodian: Data<ModelCustodian>) -> impl Responder {
-    let names = model_custodian
-        .get_ref()
-        .clone()
-        .get_empire_names()
-        .await
-        .expect("Could not get empire names");
-
-    HttpResponse::Ok().json(names)
-}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -55,49 +45,4 @@ async fn main() -> std::io::Result<()> {
     };
 
     server.run().await
-}
-
-//These are int tests in disguise.
-#[cfg(test)]
-mod tests {
-
-    use std::sync::mpsc::channel;
-
-    use crate::empires;
-    use actix_web::{body::Body, test, web::Data, App};
-    use data_model::{Budget, CustodianMsg, EmpireData, ModelCustodian, ModelDataPoint, Resources};
-    use serde_json::json;
-
-    #[actix_rt::test]
-    async fn test_empires__from_custodian__returns_list_of_empire_names() {
-        let expected_empire_names = vec![String::from("NAME")];
-
-        let (sender, receiver) = channel();
-
-        sender
-            .send(CustodianMsg::Data(ModelDataPoint {
-                campaign_name: String::new(),
-                empires: vec![EmpireData {
-                    name: String::from("NAME"),
-                    budget: Budget::default(),
-                    resources: Resources::default(),
-                }],
-            }))
-            .unwrap();
-
-        let custodian = ModelCustodian::create(receiver);
-
-        let mut app =
-            test::init_service(App::new().app_data(Data::new(custodian)).service(empires)).await;
-        let req = test::TestRequest::with_header("content-type", "application/json")
-            .uri("/empires")
-            .to_request();
-
-        let mut resp = test::call_service(&mut app, req).await;
-
-        let body = resp.take_body();
-        let body = body.as_ref().unwrap();
-        assert!(resp.status().is_success());
-        assert_eq!(&Body::from(json!(expected_empire_names.clone())), body);
-    }
 }
