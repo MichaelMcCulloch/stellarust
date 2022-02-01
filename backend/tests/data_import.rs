fa242ce22ba59eb05442a777149e68d58a898cec
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2022-02-01 13:04:58 -0700

    it all compiles now, but it does not work

--- backend/tests/data_import.rs
+++ backend/tests/data_import.rs
@@ -3,42 +3,44 @@ mod data_import_tests {
     use std::{fs, path::PathBuf, thread, time::Duration};
 
     use backend::dirwatcher::DirectoryEventHandler;
-    use data_model::ModelCustodian;
+    use data_core::DataCore;
     use sqlx::SqlitePool;
-    use test_helper::utility::{
-        cleanup_sqlite, create_sqlite_db, drop_sqlite_db, get_path, get_test_campaign_une_root,
-    };
+    use test_helper::{cleanup_sqlite, create_sqlite_db, get_path, get_test_campaign_une_root};
 
     const TEST_WORKING_DIRECTORY_SOURCE: &str = "stellarust/res/test_data/data_import/";
+    const FRESH_DB: &str = "FRESH_DB";
+    const EXISTING_DB: &str = "EXISTING_DB";
 
     #[actix_rt::test]
     async fn on_data_import__fresh_database_and_new_data__db_is_populated() {
+        let testing_data_path = &get_test_campaign_une_root();
         let test_working_dir_path = &get_path(TEST_WORKING_DIRECTORY_SOURCE);
 
         fs::create_dir_all(test_working_dir_path.clone()).unwrap();
 
-        let sqlite_db_path =
-            PathBuf::from_iter(vec![test_working_dir_path, PathBuf::from("fresh_db")]);
-
         let (receiver, _dir_watcher) = DirectoryEventHandler::create(&testing_data_path);
         // TODO: Factor out to a database custodian
-        create_sqlite_db(&sqlite_db_path);
-        let pool = SqlitePool::connect(&sqlite_db_path.to_str().unwrap());
 
-        let custodian = ModelCustodian::create(receiver);
+        let db_location = PathBuf::from_iter(vec![test_working_dir_path, &PathBuf::from(FRESH_DB)]);
+        let core = DataCore::create(&test_working_dir_path, &FRESH_DB)
+            .await
+            .unwrap();
 
-        cleanup_sqlite(&test_working_dir_path, &EXISTING_DB);
+        let pool = SqlitePool::connect(&db_location.to_str().unwrap());
+
+        cleanup_sqlite(&test_working_dir_path, &FRESH_DB);
     }
 
     #[actix_rt::test]
     async fn on_data_import__existing_database_and_same_data__db_is_untouched() {
+        let testing_data_path = &get_test_campaign_une_root();
         let test_working_dir_path = &get_path(TEST_WORKING_DIRECTORY_SOURCE);
 
-        let sqlite_db_path =
-            PathBuf::from_iter(vec![test_working_dir_path, PathBuf::from("existing_db")]);
-        create_sqlite_db(&sqlite_db_path).unwrap();
+        let db_location =
+            PathBuf::from_iter(vec![test_working_dir_path, &PathBuf::from(EXISTING_DB)]);
+        create_sqlite_db(&test_working_dir_path, &EXISTING_DB).unwrap();
 
-        let poolA = SqlitePool::connect(&sqlite_db_path.to_str().unwrap())
+        let poolA = SqlitePool::connect(&db_location.to_str().unwrap())
             .await
             .unwrap();
 
